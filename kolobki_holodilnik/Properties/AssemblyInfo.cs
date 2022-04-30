89b6f79de42b782bbc8ddcf6b4d63812b96696b3
Антон Забабурин <asumosan@gmail.com>
2022-04-30 19:57:09 +0500

    Add icon, and rename

--- kolobki_holodilnik/Properties/AssemblyInfo.cs
+++ kolobki_holodilnik/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("kolobki_holodilnik")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Fridge")]
+[assembly: AssemblyDescription("Pazl from old game")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("kolobki_holodilnik")]
+[assembly: AssemblyProduct("Fridge")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-US")]
